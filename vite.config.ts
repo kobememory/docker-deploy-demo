f13e7fd7673665f4f9c5d7df15ed47441d29299f
lankobe <12688549+lankobe@user.noreply.gitee.com>
2023-03-31 12:42:49 +0800

    Update nginx.conf

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/demo',
+  publicDir: '/demo',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
